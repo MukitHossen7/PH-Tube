088a6692bdb44a29f2498cec38a63376df22fec1
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-02 22:44:56 +0600

    dynamiclly videos add

--- js/index.js
+++ js/index.js
@@ -17,7 +17,35 @@ const displayCategory = (categories) => {
     showCategory.appendChild(button);
   });
 };
+// Create Dynamic Video Sections
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const displayVideos = (videos) => {
+  const cardSection = document.getElementById("card-section");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = /*html*/ `
+      <figure class="w-full h-52">
+    <img src="${video.thumbnail}" alt = "Videos" class="w-full h-full object-cover" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+      `;
+    cardSection.append(card);
+  });
+};
 
 loadCategory();
-
-// Create Dynamic Video Sections
+loadVideos();
